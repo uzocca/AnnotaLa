1f7e9f6cdb60f808e440fa24ef34354e2fc37770
uzocca <u.zocca@gmail.com>
2026-04-11 15:22:45 +0200

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,12 +1,8 @@
-const CACHE = 'annotatala-v1';
-const ASSETS = ['./', './index.html', './manifest.json'];
-
-self.addEventListener('install', e => {
-  e.waitUntil(caches.open(CACHE).then(c => c.addAll(ASSETS)));
-});
-
-self.addEventListener('fetch', e => {
-  e.respondWith(
-    caches.match(e.request).then(r => r || fetch(e.request))
+self.addEventListener('install', () => self.skipWaiting());
+self.addEventListener('activate', e => {
+  e.waitUntil(
+    caches.keys().then(keys => Promise.all(keys.map(k => caches.delete(k))))
+    .then(() => self.clients.claim())
   );
 });
+self.addEventListener('fetch', e => e.respondWith(fetch(e.request)));
